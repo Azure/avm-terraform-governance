993077745bbd774bc93e7aaeb03ddc0068338354
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-06-18 09:58:29 +0100

    feat: update umi fed creds with job workflow ref

--- tf-repo-mgmt/repository_sync/locals.tf
+++ tf-repo-mgmt/repository_sync/locals.tf
@@ -4,6 +4,11 @@ locals {
 }
 
 locals {
+  gh_actions_job_workflow_ref_claim_suffix = contains(
+    local.preview_gh_actions_oidc_subject_claim_customization_repos,
+    data.github_repository.this.name,
+  ) ? ":job_workflow_ref:Azure/avm-terraform-governance/.github/workflows/pr-check-template.yml@refs/heads/main" : ""
+
   preview_gh_actions_oidc_subject_claim_customization_repos = toset([
     "terraform-azure-avm-utl-interfaces",
     "terraform-azurerm-avm-res-keyvault-vault"
