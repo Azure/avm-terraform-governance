40b0d9aba727b6f0d15121b5170b5ff4b20c407c
Jared Holgate <jaredholgate@microsoft.com>
2025-06-18 13:18:39 +0100

    backfill csv

--- tf-repo-mgmt/scripts/manual/Invoke-AvmRepoMetaDataBackfill.ps1
+++ tf-repo-mgmt/scripts/manual/Invoke-AvmRepoMetaDataBackfill.ps1
@@ -1,16 +1,17 @@
 param(
-  $client_id, # This is the Client ID of the GitHub App
-  $private_key_path = "azure-verified-modules.pem" # This is the path to the private key for the GitHub App
+  $client_id = "Iv23lihQ3SvHOms4F7ym", # This is the Client ID of the GitHub App
+  $private_key_path = "azure-verified-modules.pem", # This is the path to the private key for the GitHub App
+  $metaDataFilePath = "./repository-meta-data/meta-data.csv" # This is the path to the meta data CSV file
 )
 
 # Authenticate with GitHub CLI using the GitHub App
-./scripts/Connect-AsApp.ps1 -client_id $client_id -private_key_path $private_key_path
+./scripts/manual/Connect-AsApp.ps1 -client_id $client_id -private_key_path $private_key_path
 
 # Get the CSV Files
-./scripts/Invoke-AvmRepoCsvDownload.ps1
+./scripts/manual/Invoke-AvmRepoCsvDownload.ps1
 
 # Get the list of installed repositories for the GitHub App
-$repositories = ./scripts/Invoke-RepoSyncGenerateMatrix.ps1
+$repositories = ./scripts/Get-RepositoriesWhereAppInstalled.ps1
 
 $resourceModulesCSVData = Import-Csv -Path "./temp/TerraformResourceModules.csv"
 $patternModulesCSVData = Import-Csv -Path "./temp/TerraformPatternModules.csv"
@@ -19,42 +20,39 @@ $utilityModulesCSVData = Import-Csv -Path "./temp/TerraformUtilityModules.csv"
 $metaDataVariables = @(
   @{
       key = "ProviderNamespace"
-      name = "AVM_RESOURCE_PROVIDER_NAMESPACE"
+      name = "providerNamespace"
   },
   @{
       key = "ResourceType"
-      name = "AVM_RESOURCE_TYPE"
+      name = "providerResourceType"
   },
   @{
       key = "ModuleDisplayName"
-      name = "AVM_MODULE_DISPLAY_NAME"
+      name = "moduleDisplayName"
   },
   @{
       key = "AlternativeNames"
-      name = "AVM_MODULE_ALTERNATIVE_NAMES"
-  },
-  @{
-      key = "Comments"
-      name = "AVM_COMMENTS"
+      name = "alternativeNames"
   },
   @{
       key = "PrimaryModuleOwnerGHHandle"
-      name = "AVM_OWNER_PRIMARY_GITHUB_HANDLE"
+      name = "primaryOwnerGitHubHandle"
   },
   @{
       key = "PrimaryModuleOwnerDisplayName"
-      name = "AVM_OWNER_PRIMARY_DISPLAY_NAME"
+      name = "primaryOwnerDisplayName"
   },
   @{
       key = "SecondaryModuleOwnerGHHandle"
-      name = "AVM_OWNER_SECONDARY_GITHUB_HANDLE"
+      name = "secondaryOwnerGitHubHandle"
   },
   @{
       key = "SecondaryModuleOwnerDisplayName"
-      name = "AVM_OWNER_SECONDARY_DISPLAY_NAME"
+      name = "secondaryOwnerDisplayName"
   }
 )
 
+$csvData = @()
 foreach($repository in $repositories) {
   $repositoryCSVData = $null
 
@@ -75,23 +73,28 @@ foreach($repository in $repositories) {
 
   Write-Host "Repository $($repository.repoId) found in CSV data" -ForegroundColor Green
 
-  foreach($item in $metaDataVariables) {
-    if(!$repositoryCSVData.PSObject.Properties.Name -contains $item.key) {
-      Write-Host "Meta data item $($item.name) not found in CSV data for: $($repository.repoId)"
-      continue
+  # Clean up repository variables
+  $respositoryVariables = gh variable list --repo "$($repository.repoUrl)" --json "name" | ConvertFrom-Json
+  foreach($variable in $respositoryVariables) {
+    if($variable.name -like "AVM_*") {
+      Write-Host "Removing existing variable: $($variable.name) from repository: $($repository.repoId)"
+      gh variable delete "$($variable.name)" --repo "$($repository.repoUrl)"
     }
-    $metaDataItem = $repositoryCSVData.($item.key)
-
-    Write-Host "Meta data item $($item.name) found for: $($repository.repoId)"
-
-    if($null -eq $metaDataItem -or $metaDataItem -eq "") {
-      Write-Host "Meta data item $($item.name) is null or empty for: $($repository.repoId)"
-      continue
-    }
-
-    # Set the variable in the GitHub repository
-    $cliCommand = "gh variable set `"$($item.name)`" --body `"$metaDataItem`" --repo `"$($repository.repoUrl)`""
-    Write-Host "Running: $cliCommand" -ForegroundColor Blue
-    gh variable set "$($item.name)" --body "$metaDataItem" --repo "$($repository.repoUrl)"
   }
+
+  $csvRow = [ordered]@{}
+  $csvRow["moduleId"] = $repository.repoId
+  foreach($item in $metaDataVariables) {
+    $metaDataItem = ""
+    if($repositoryCSVData.PSObject.Properties.Name -contains $item.key) {
+      $metaDataItem = $repositoryCSVData.($item.key)
+      Write-Host "Meta data item $($item.name) found for: $($repository.repoId)"
+    }
+
+    $csvRow[$item.name] = $metaDataItem
+  }
+  $csvData += $csvRow
 }
+
+# Write the updated meta data to the CSV file
+$csvData | Export-Csv -Path $metaDataFilePath -NoTypeInformation -Encoding UTF8
