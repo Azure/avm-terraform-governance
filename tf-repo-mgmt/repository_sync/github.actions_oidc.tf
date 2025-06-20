fc90da50aac0cbcd462ecb5166ebe28fa4d5c1dd
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-06-20 15:53:28 +0100

    feat: update GitHub Actions workflow and enhance OIDC subject claim to use managed workflow (#7)

---
+++ tf-repo-mgmt/repository_sync/github.actions_oidc.tf
@@ -0,0 +1,12 @@
+# This file manages the GitHub Actions OIDC subject claim customization for specific repositories
+# that are in-scope. When wanting to roll out to all repositories, set count to 1 and remove the condition.
+resource "github_actions_repository_oidc_subject_claim_customization_template" "this" {
+  count       = contains(local.preview_gh_actions_oidc_subject_claim_customization_repos, data.github_repository.this.name) ? 1 : 0
+  repository  = data.github_repository.this.name
+  use_default = false
+  include_claim_keys = [
+    "repository",
+    "environment",
+    "job_workflow_ref",
+  ]
+}
