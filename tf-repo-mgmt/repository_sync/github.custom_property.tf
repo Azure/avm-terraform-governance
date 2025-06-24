e56f5aa40a278bea133bce8a784d143f629035e1
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-06-24 15:53:44 +0100

    fix: repo name

--- tf-repo-mgmt/repository_sync/github.custom_property.tf
+++ tf-repo-mgmt/repository_sync/github.custom_property.tf
@@ -3,7 +3,7 @@ resource "github_repository_custom_property" "prod_opt_in" {
     local.preview_gh_actions_oidc_subject_claim_customization_repos,
     data.github_repository.this.name,
   ) ? 1 : 0
-  repository     = github_repository.example.name
+  repository     = data.github_repository.this.name
   property_name  = "rulesets-prod-opt-in"
   property_type  = "single_select"
   property_value = ["false"]
@@ -14,7 +14,7 @@ resource "github_repository_custom_property" "default_opt_in" {
     local.preview_gh_actions_oidc_subject_claim_customization_repos,
     data.github_repository.this.name,
   ) ? 1 : 0
-  repository     = github_repository.example.name
+  repository     = data.github_repository.this.name
   property_name  = "rulesets-default-opt-in"
   property_type  = "single_select"
   property_value = ["false"]
