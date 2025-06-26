24a8c4777ce00bfec8e4c85eec6d8f7089340c1b
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-06-26 11:03:51 +0100

    feat(governance): regular job to run pre-commit

--- tf-repo-mgmt/repository_sync/github.custom_property.tf
+++ tf-repo-mgmt/repository_sync/github.custom_property.tf
@@ -1,6 +1,6 @@
 resource "github_repository_custom_property" "prod_opt_in" {
   count = contains(
-    local.preview_gh_actions_oidc_subject_claim_customization_repos,
+    local.preview_ruleset_bypass_for_app_repos,
     data.github_repository.this.name,
   ) ? 1 : 0
   repository     = data.github_repository.this.name
@@ -11,11 +11,22 @@ resource "github_repository_custom_property" "prod_opt_in" {
 
 resource "github_repository_custom_property" "default_opt_in" {
   count = contains(
-    local.preview_gh_actions_oidc_subject_claim_customization_repos,
+    local.preview_ruleset_bypass_for_app_repos,
     data.github_repository.this.name,
   ) ? 1 : 0
   repository     = data.github_repository.this.name
   property_name  = "rulesets-default-opt-in"
   property_type  = "single_select"
   property_value = ["false"]
+}
+
+resource "github_repository_custom_property" "global_opt_out" {
+  count = contains(
+    local.preview_ruleset_bypass_for_app_repos,
+    data.github_repository.this.name,
+  ) ? 1 : 0
+  repository     = data.github_repository.this.name
+  property_name  = "global-rulesets-opt-out"
+  property_type  = "single_select"
+  property_value = ["true"]
 }
