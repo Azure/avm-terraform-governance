a8a7bd0f3d1c4cb593ae638ff359fe7c3060863b
Jared Holgate <jaredholgate@microsoft.com>
2025-07-03 12:37:09 +0100

    linting

--- tf-repo-mgmt/repository_sync/github.teams.tf
+++ tf-repo-mgmt/repository_sync/github.teams.tf
@@ -1,14 +1,14 @@
 data "github_team" "this" {
   for_each = var.github_teams
-  slug  = each.value.slug
+  slug     = each.value.slug
 }
 
 locals {
-  repository_teams = { for k,v in var.github_teams : k => {
-    id = data.github_team.this[k].id
+  repository_teams = { for k, v in var.github_teams : k => {
+    id         = data.github_team.this[k].id
     permission = v.repository_access_permission
-   } if v.repository_access_permission != "none" }
-  environment_approval_teams = { for k,v in var.github_teams : k => data.github_team.this[k].id if v.environment_approval }
+  } if v.repository_access_permission != "none" }
+  environment_approval_teams = { for k, v in var.github_teams : k => data.github_team.this[k].id if v.environment_approval }
 }
 
 resource "github_team_repository" "this" {
