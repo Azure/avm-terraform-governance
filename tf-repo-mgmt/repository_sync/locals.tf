af8f16b160f922ac552bfccd4fed980494e7a753
Jared Holgate <jaredholgate@microsoft.com>
2025-07-08 17:41:25 +0100

    trim space from labels to avoid replaces

--- tf-repo-mgmt/repository_sync/locals.tf
+++ tf-repo-mgmt/repository_sync/locals.tf
@@ -10,8 +10,8 @@ locals {
 locals {
   label_list = csvdecode(file(var.github_labels_source_path))
   labels = { for label in local.label_list : label.Name => {
-    name        = label.Name
+    name        = trimspace(label.Name)
     color       = label.HEX
-    description = strcontains(label.Description, ":") ? trimspace(replace(split(":", split(".", label.Description)[0])[1], "this", "This")) : label.Description
+    description = strcontains(label.Description, ":") ? trimspace(replace(split(":", split(".", label.Description)[0])[1], "this", "This")) : trimspace(label.Description)
   } }
 }
