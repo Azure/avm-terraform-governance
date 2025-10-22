b60a6711221cf5e7180869882e38f7e23f7407b3
Jared Holgate <jaredholgate@microsoft.com>
2025-10-22 16:59:56 +0100

    typo

--- tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.copilot.tf
+++ tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.copilot.tf
@@ -15,7 +15,7 @@ resource "github_actions_environment_secret" "copilot_tenant_id" {
 resource "github_actions_environment_secret" "copilot_subscription_id" {
   count           = var.repository_creation_mode_enabled ? 0 : 1
   repository      = github_repository.this.name
-  environment     = github_repository_environment.this[0].environment
+  environment     = github_repository_environment.copilot[0].environment
   secret_name     = "ARM_SUBSCRIPTION_ID"
   plaintext_value = var.test_subscription_ids[0].id
 }
