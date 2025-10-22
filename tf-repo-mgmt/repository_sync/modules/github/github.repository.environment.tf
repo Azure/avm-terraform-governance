4711de6db7b90c3d6b6dbc3f2fe07f10f9ca9649
Jared Holgate <jaredholgate@microsoft.com>
2025-10-22 23:19:19 +0100

    fix tests

--- tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
+++ tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
@@ -21,14 +21,6 @@ resource "github_actions_environment_secret" "tenant_id" {
   plaintext_value = var.arm_tenant_id
 }
 
-resource "github_actions_environment_secret" "subscription_id" {
-  count           = var.repository_creation_mode_enabled ? 0 : 1
-  repository      = github_repository.this.name
-  environment     = github_repository_environment.this[0].environment
-  secret_name     = "ARM_SUBSCRIPTION_ID"
-  plaintext_value = var.test_subscription_ids[0].id
-}
-
 resource "github_actions_environment_secret" "test_subscription_ids" {
   count           = var.repository_creation_mode_enabled ? 0 : 1
   repository      = github_repository.this.name
