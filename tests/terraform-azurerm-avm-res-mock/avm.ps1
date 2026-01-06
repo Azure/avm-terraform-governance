1e1f262e79e029e97fd8241dd04c56e51ea80dfb
Jared Holgate <jaredholgate@microsoft.com>
2026-01-06 17:12:08 +0000

    fix avm files

--- tests/terraform-azurerm-avm-res-mock/avm.ps1
+++ tests/terraform-azurerm-avm-res-mock/avm.ps1
@@ -130,6 +130,7 @@ if (-not $env:AVM_IN_CONTAINER) {
     "MPTF_URL",
     "NO_COLOR",
     "PORCH_LOG_LEVEL",
+    "AVM_PORCH_STDOUT",
     "TEST_TYPE",
     "TFLINT_CONFIG_URL"
   )
@@ -169,6 +170,10 @@ if (-not $env:AVM_IN_CONTAINER) {
 
   if ($TUI) {
     $dockerArgs += "TUI=$TUI"
+  }
+
+  if($env:AVM_PORCH_STDOUT) {
+    $dockerArgs += "AVM_PORCH_STDOUT=$($env:AVM_PORCH_STDOUT)"
   }
 
   $dockerArgs += "MAKEFILE_REF=$MAKEFILE_REF"
