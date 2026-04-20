2eb48baa18593aa918a2038f2a31d86390dd8837
Jared Holgate <jaredholgate@microsoft.com>
2026-04-20 09:58:55 +0100

    Refactor GitHub Actions environment secrets configuration

--- tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
+++ tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
@@ -21,20 +21,26 @@ resource "github_actions_environment_secret" "tenant_id" {
   plaintext_value = var.arm_tenant_id
 }
 
-resource "github_actions_environment_secret" "test_subscription_ids" {
-  count           = var.repository_creation_mode_enabled ? 0 : 1
-  repository      = github_repository.this.name
-  environment     = github_repository_environment.this[0].environment
-  secret_name     = "TEST_SUBSCRIPTION_IDS"
-  plaintext_value = jsonencode(var.test_subscription_ids)
-}
-
 resource "github_actions_environment_secret" "client_id" {
   count           = var.repository_creation_mode_enabled ? 0 : 1
   repository      = github_repository.this.name
   environment     = github_repository_environment.this[0].environment
   secret_name     = "ARM_CLIENT_ID"
   plaintext_value = var.arm_client_id
+}
+
+resource "github_repository_environment" "test_no_approval" {
+  count               = var.repository_creation_mode_enabled ? 0 : 1
+  environment         = var.github_repository_test_no_approval_environment_name
+  repository          = github_repository.this.name
+}
+
+resource "github_actions_environment_secret" "test_subscription_ids" {
+  count           = var.repository_creation_mode_enabled ? 0 : 1
+  repository      = github_repository.this.name
+  environment     = github_repository_environment.test_no_approval[0].environment
+  secret_name     = "TEST_SUBSCRIPTION_IDS"
+  plaintext_value = jsonencode(var.test_subscription_ids)
 }
 
 # This environment is used for jobs that do not require authentication.
