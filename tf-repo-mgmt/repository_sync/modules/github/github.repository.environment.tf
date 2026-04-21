935cbeaf14489e5c83a9886b98771e46a12ce8bd
Jared Holgate <jaredholgate@microsoft.com>
2026-04-21 13:07:18 +0100

    refactor: move repo-scope actions secrets into dedicated file
    These secrets are no longer aligned to environments, so split them out of github.repository.environment.tf into github.actions_secrets.tf for clarity.

--- tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
+++ tf-repo-mgmt/repository_sync/modules/github/github.repository.environment.tf
@@ -31,27 +31,3 @@ resource "github_repository_environment" "no_approval" {
   environment = var.github_repository_no_approval_environment_name
   repository  = github_repository.this.name
 }
-
-# Shared Azure auth values are stored at repository scope so they can be
-# consumed by any environment (and any new environment added in future)
-# without duplicating per-environment secrets.
-resource "github_actions_secret" "arm_tenant_id" {
-  count           = var.repository_creation_mode_enabled ? 0 : 1
-  repository      = github_repository.this.name
-  secret_name     = "ARM_TENANT_ID"
-  plaintext_value = var.arm_tenant_id
-}
-
-resource "github_actions_secret" "arm_client_id" {
-  count           = var.repository_creation_mode_enabled ? 0 : 1
-  repository      = github_repository.this.name
-  secret_name     = "ARM_CLIENT_ID"
-  plaintext_value = var.arm_client_id
-}
-
-resource "github_actions_secret" "test_subscription_ids" {
-  count           = var.repository_creation_mode_enabled ? 0 : 1
-  repository      = github_repository.this.name
-  secret_name     = "TEST_SUBSCRIPTION_IDS"
-  plaintext_value = jsonencode(var.test_subscription_ids)
-}
